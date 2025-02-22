8ff03cccf67e19678abd0b42ba22206750b597fd
Ziead Ahmed <zyeadahmed629@gmail.com>
2025-02-22 16:20:49 +0200

    Add files via upload

---
+++ Sequential/sequential1.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <fstream> 
+
+using namespace std; 
+ 
+int main() { 
+ char ch; 
+ fstream file; 
+ char filename[20]; 
+ cout << "Enter the name of the file: " << flush; 
+ cin >> filename; 
+ file.open(filename, ios::in); 
+ file.unsetf(ios::skipws); 
+ while (1) 
+ { 
+  file >> ch; 
+  if (file.fail()) 
+   break; 
+  cout << ch; 
+ } 
+ file.close(); 
+ return 0;
+} 
